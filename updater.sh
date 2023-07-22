201fb868d99a63545611f1ac36c391fae09bdb3a
Hennetin Hugo <zerbaib@proton.me>
2023-07-22 10:34:07 +0200

    😎 Updater

---
+++ updater.sh
@@ -0,0 +1,4 @@
+sudo apt-get remove appimagebuilder -y
+sudo wget https://github.com/Zerbaib/AppImageBuilder/releases/download/v1.9/AppImageBuilder.deb
+sudo dpkg -i AppImageBuilder.deb
+sudo rm -R AppImageBuilder.deb
