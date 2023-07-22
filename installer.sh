87877f37aaceeac4398fb2696a8b8f72032a46bb
Hennetin Hugo <zerbaib@proton.me>
2023-07-22 10:30:56 +0200

    😎 Done

--- installer.sh
+++ installer.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Download the .deb file
 sudo wget https://github.com/Zerbaib/AppImageBuilder/releases/download/v1.9/AppImageBuilder.deb
+
+# Install the .deb file
 sudo dpkg -i AppImageBuilder.deb
+
+# Clean up by removing the .deb file
 sudo rm -R AppImageBuilder.deb
